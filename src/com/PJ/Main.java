44427211bd56dbf75c25c73092bd0104af88cb6f
Fizka <olaluzaczka@wp.pl>
2019-10-30 12:06:03 +0100

    new files

---
+++ src/com/PJ/Main.java
@@ -0,0 +1,12 @@
+package com.PJ;
+
+import java.awt.*;
+
+public class Main {
+
+    public static Data basedate;
+
+    public static void main(String[] args) {
+        basedate = new Data(); 
+    }
+}
